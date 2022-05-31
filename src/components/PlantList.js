839139aeaceafe98096c9d6092695c2c8a3e28e8
John <johnmulvey48@yahoo.com>
2022-05-31 10:59:49 -0400

    git@github.com:Jmulvez/react-hooks-mock-code-challenge-plantshop.git

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  useEffect(() => {
+    fetch("http://localhost:6001")
+    .then((e) => e.json())
+    .then(setData);
+  }, []);
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      <p>{id}</p>
+      <p>{name}</p>
+      <img>{image}</img>
+      <p>{price}</p>
+    </ul>
   );
 }
 
